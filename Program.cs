64e2f2ca3dee21eb28a693f383bcbcf3eac193dd
Max Nosov <mnosov88@gmail.com>
2020-11-24 13:43:00 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestFolder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
